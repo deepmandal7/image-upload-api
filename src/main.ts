db3aef59e8b81a5f2a6b666a284a67d3f2c5ffbb
Deep Mandal <deepmandal4742@gmail.com>
2022-11-19 20:29:58 +0530

    Fixed added port to env

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
     origin: '*',
   });
   app.register(fmp, { attachFieldsToBody: true });
-  await app.listen(8000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
