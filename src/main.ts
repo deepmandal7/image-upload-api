c998625737b74f687bea04e6a1a4fba8b3d725ca
Deep Mandal <deepmandal4742@gmail.com>
2022-11-19 21:00:53 +0530

    Fixed app.listen

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
     origin: '*',
   });
   app.register(fmp, { attachFieldsToBody: true });
-  await app.listen(process.env.PORT || 8000);
+  await app.listen(process.env.PORT || 8000, '0.0.0.0');
 }
 bootstrap();
